6bd8faec381cad6839dd6bbc6ce31b3fdd728b14
SchoolTARMaster <153262719+SchoolTARMaster@users.noreply.github.com>
2025-12-25 15:45:45 +0200

    Update unityUrls.js

--- js/unityUrls.js
+++ js/unityUrls.js
@@ -2,12 +2,13 @@ window.loadedUrls = true;
 const hoursBeforeMandatoryRefresh = 6;
 
 function getBuildUrl() {
-	return 'https://media.githubusercontent.com/SchoolTARMaster/middlechild/raw/refs/heads/main/cdn/Build' ;
+	return 'https://media.githubusercontent.com/media/SchoolTARMaster/middlechild/refs/heads/main/cdn/Build' ;
 }
 
 function getGameLoaderUrl() {
 	return "cdn/Build/WebGL.loader.js";
 }
+
 
 
 
